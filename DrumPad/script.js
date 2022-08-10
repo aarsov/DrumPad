76bf63cfff2c8f2f48713b433de333fd24964580
aarsov <69254480+aarsov@users.noreply.github.com>
2022-08-10 12:38:24 +0200

    Add files via upload

---
+++ DrumPad/script.js
@@ -0,0 +1,44 @@
+function play1() {
+    let audio1 = document.getElementById("audio1");
+    audio1.play()
+}
+
+function play2() {
+    let audio2 = document.getElementById("audio2");
+    audio2.play();
+}
+
+function play3() {
+    let audio3 = document.getElementById("audio3");
+    audio3.play();
+}
+
+function play4() {
+    let audio4 = document.getElementById("audio4"); 
+    audio4.play();
+}
+
+function play5() {
+    let audio5 = document.getElementById("audio5");
+    audio5.play();
+}
+
+function play6() {
+    let audio6 = document.getElementById("audio6");
+    audio6.play();
+}
+
+function play7() {
+    let audio7 = document.getElementById("audio7");
+    audio7.play();
+}
+
+function play8() {
+    let audio8 = document.getElementById("audio8");
+    audio8.play();
+}
+
+function play9() {
+    let audio9 = document.getElementById("audio9");
+    audio9.play();
+}
